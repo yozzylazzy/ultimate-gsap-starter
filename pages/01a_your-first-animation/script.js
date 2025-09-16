185418192a175d69fd653c0995eca100fd92b5cc
yozzylazzy <kristian_yosef@yahoo.com>
2025-09-16 09:58:19 +0700

    update

--- pages/01a_your-first-animation/script.js
+++ pages/01a_your-first-animation/script.js
@@ -9,13 +9,5 @@ gsap.to(".card", {
       yoyo: true,
       duration: 0.5,
     });
-
-    // Pulsing glow animation here
-    gsap.to(".card", {
-      boxShadow: "0 20px 80px rgba(29,209,161,0.6)",
-      repeat: -1,
-      yoyo: true,
-      duration: 1,
-    });
   },
 });
